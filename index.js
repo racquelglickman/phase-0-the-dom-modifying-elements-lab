d37a83a33d11ff58dd51369e4efa5cc33460e883
Racquel Glickman <racquel.glickman@gmail.com>
2023-03-14 13:40:39 -0400

    completes dom modifying lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+newHeader.textContent = "Racquel is the champion";
+document.body.append(newHeader);
